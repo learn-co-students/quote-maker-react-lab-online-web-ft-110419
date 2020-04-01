2bf4c261c5a9770b6737dec7789580824373cfdb
Gilblasse <gilblasse@gmail.com>
2020-04-01 03:05:52 -0400

    Done.

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,39 @@
+import {
+  ADD_QUOTE,
+  REMOVE_QUOTE,
+  UPVOTE_QUOTE,
+  DOWNVOTE_QUOTE
+} from "../actions/quoteTypes";
+
 export default (state = [], action) => {
-  return state;
-}
+  switch (action.type) {
+    case ADD_QUOTE:
+      return [...state, action.quote];
+
+    case REMOVE_QUOTE:
+      return state.filter(quote => quote.id !== action.quoteId);
+
+    case UPVOTE_QUOTE:
+      return state.map(quote => {
+        return quote.id === action.quoteId
+          ? { ...quote, vote: quote.vote + 1 }
+          : quote;
+      });
+
+    case DOWNVOTE_QUOTE:
+      return state.map(quote => {
+        if (
+          quote.id === action.quoteId &&
+          quote.votes >= 1 &&
+          typeof quote.votes === "number"
+        ) {
+          return { ...quote, vote: quote.vote - 1 };
+        } else {
+          return quote;
+        }
+      });
+
+    default:
+      return state;
+  }
+};
