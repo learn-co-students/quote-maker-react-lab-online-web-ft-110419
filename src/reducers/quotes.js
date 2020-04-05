16ea7c453238c86acedc7f5f75a71dd705ecafc0
LucyWeltner <lucyiweltner@gmail.com>
2020-04-05 15:04:55 -0400

    Done.

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,27 @@
 export default (state = [], action) => {
-  return state;
+	switch (action.type) {
+		case 'ADD_QUOTE':
+			let newState = state.slice()
+			newState.push(action.quote)
+			return newState
+		case 'REMOVE_QUOTE':
+			console.log("remove")
+			index = state.findIndex(quote => quote.id === action.quoteId)
+			return (state.slice(0, index)).concat(state.slice(index+1))
+		case 'UPVOTE_QUOTE':
+			console.log("upvote")
+			let index = state.findIndex(quote => quote.id === action.quoteId)
+			newState = state.slice()
+			newState[index].votes += 1
+			return newState
+		case 'DOWNVOTE_QUOTE':
+			index = state.findIndex(quote => quote.id === action.quoteId)
+			newState = state.slice()
+			if (newState[index].votes > 0) {
+				newState[index].votes -= 1
+			}
+			return newState
+		default:
+			return state
+	}
 }
