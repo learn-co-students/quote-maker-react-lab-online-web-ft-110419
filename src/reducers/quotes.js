50ce01c231f97b21fa96182f9f7b27dbe5b137ca
JesseGonzalez024 <jessegonzalez024@gmail.com>
2020-04-10 17:27:40 -0400

    Done.

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,28 @@
+// The action argument is coming from the QuotesForm, defined inside the mapDispatchToProps
+// But called on the form's handle Submit
+
+// The React store holds the state for the entire App
+// The QuotesForm has it's own seperate state.
+
 export default (state = [], action) => {
-  return state;
+  console.log(action, "Inside Reducer")
+
+  switch ( action.type ) {
+    case "ADD_QUOTE": 
+      return [...state, action.quote]
+    case "REMOVE_QUOTE": 
+      return state.filter( quote => quote.id !== action.quoteId )
+    case "UPVOTE_QUOTE":
+      state.find( quote => quote.id === action.quoteId ).votes ++ 
+      return state 
+    case "DOWNVOTE_QUOTE":
+      let quote = state.find( quote => quote.id === action.quoteId )
+      if (quote.votes > 0) {
+        quote.votes -- 
+      }  
+      return state 
+    default: 
+      return state 
+  }
+  
 }
