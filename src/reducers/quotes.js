3b8da58ded4d4822d437b3351bde5f3179dccd8f
Bryant Richards <dragonseyeimages@gmail.com>
2020-04-14 17:48:55 -0500

    Done.

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,20 @@
 export default (state = [], action) => {
-  return state;
+  debugger
+  switch ( action.type ) {
+    case "ADD_QUOTE":
+      return [...state, action.quote]
+    case "REMOVE_QUOTE":
+      return state.filter( quote => quote.id !== action.quoteId )
+    case "UPVOTE_QUOTE":
+      state.find( quote => quote.id === action.quoteId ).votes ++
+      return state
+    case "DOWNVOTE_QUOTE":
+      let quote = state.find( quote => quote.id === action.quoteId )
+      if (quote.votes > 0) {
+        quote.votes --
+      }
+      return state
+    default:
+      return state
+  }
 }
